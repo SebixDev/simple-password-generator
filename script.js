bb4b1681ccc37fdc34510a3c02f1bce1534697c3
sebixDev <you@example.com>
2026-01-13 17:44:05 +0100

    JavaScript erfolgreich in script.js ausgelagert

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelector("#generate").addEventListener("click", function() {
+            var password = generatePassword(12);
+           document.querySelector("#Password").innerHTML = password;
+        });
+
+        function generatePassword(count) {
+            var generatedPassword = "";
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+            for (i = 0; i < count; i++) {
+                var index = zufallszahl(chars.length);
+                generatedPassword += chars [index];
+                
+            }
+            return generatedPassword;
+        }
+
+        function zufallszahl(max) {
+            return Math.floor(Math.random() * max);
+        }
